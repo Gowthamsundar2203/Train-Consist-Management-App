81b0e9ea2e271c313a4a4ebbc434c0f6cdbf8f96
S Lakshmi Gowtham <gowthamsundar2203@gmail.com>
2026-04-06 22:34:32 +0530

    Update TrainConsistManagementApp.java

--- TrainConsistManagementApp.java
+++ TrainConsistManagementApp.java
@@ -1,7 +1,7 @@
 import java.util.HashSet;
 import java.util.Set;
 
-public class UseCase3TrainConsistMgmt {
+public class TrainConsistManagementAp{
     public static void main(String[] args) {
 
         System.out.println("================================");
