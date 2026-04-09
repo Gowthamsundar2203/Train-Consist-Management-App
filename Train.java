245444f30b12f1fa2ed3338abfc99877cc1548e9
gowthamsundar2203 <gowthamsundar2203@gmail.com>
2026-04-09 15:00:08 +0530

    UC11 NEW

--- Train.java
+++ Train.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.regex.*;
 
-public class Main {
+public class Train{
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
